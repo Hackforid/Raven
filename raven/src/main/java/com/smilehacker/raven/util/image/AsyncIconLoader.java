5211b372f0e1ef9d3a76b4dd809c1be58b9d0aef
Sai <zhouquanbest@gmail.com>
2014-05-21 00:02:25 +0800

    resize cache;

--- raven/src/main/java/com/smilehacker/raven/util/image/AsyncIconLoader.java
+++ raven/src/main/java/com/smilehacker/raven/util/image/AsyncIconLoader.java
@@ -29,7 +29,7 @@ public class AsyncIconLoader {
 
     private void initCache() {
         final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
-        final int cacheSize = maxMemory / 16;
+        final int cacheSize = maxMemory / 8;
         mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
             @Override
             protected int sizeOf(String key, Bitmap value) {
